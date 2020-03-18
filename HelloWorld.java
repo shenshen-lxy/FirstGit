c15e6c6d506277084a57303fdc83faf999a114c8
shenshen <1105093577@qq.com>
2020-03-18 14:16:52 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+	public staitc void main(String args[]){
+		System.out.println("HelloWorld!");
+		System.out.println("1");
+		System.out.println("HelloWorld!");
+		System.out.println("2");
+	}
+}
